2f9aeede6667b13bf9fb27ea6e3a0e5540714aac
Code Gaucho <peter.wu13@gmail.com>
2019-03-07 19:43:21 -0800

    Updated formula!

--- src/Levels.py
+++ src/Levels.py
@@ -10,8 +10,7 @@ class Level(FloatLayout):
         self.player_pos = None
         self.pos = (0,0)
         self.blocks_arr=blocks
-        self.data = self.formatLevel(self.ParseLevel(file))
-        self.getBlocks()
+        self.blocks = self.ParseLevel()
         #assert (self.player_pos is not None), ("There is no player in the level \"{0}\"".format(name))
         for block in self.blocks:
             if block is not None:
@@ -38,44 +37,48 @@ class Level(FloatLayout):
 
         pixels=img.load()
 
-        for i in range(self.width):
-            for j in range(self.height):
-                result.append(pixels[i,j])
+        result = list()
+        for x in range(self.width):
+            for y in range(self.height):
+                block_num = self.formatPixel(pixels[x,y])
+                for block in range(len(self.blocks_arr)):
+                    if block_type is self.blocks_arr[block].number:
+                        res=Block(self.blocks_arr[block].file, self.blocks_arr[block].color)
+                        res.pos=(x*res.size[0], y*res.size[1])
+                        result.append(res)
+                        break
 
         return result
 
-    def formatLevel(self, data):
+    def formatPixel(self, pixel):
         result=[]
-        for res in data:
-            for block in self.blocks_arr:
-                r,g,b,a=res # Setting r, g, b, and a from that position in the level
-                if [r,g,b,a] == block.color+[255]: # Checking if that color matches a block
-                    result.append(block.number) # if so, append that block to the result
-                    break
-                elif [r,g,b,a] == [255,0,0,255]: # Checking if it is a 'player_start' block (red)
-                    result.append(-1)
-                #checking if this is the last block
-                if block.number+1 is Block.Index:
-                    result.append(None)
+        for block in self.blocks_arr:
+            r,g,b,a=res # Setting r, g, b, and a from that position in the level
+            if [r,g,b,a] == block.color+[255]: # Checking if that color matches a block
+                return block.number # if so, append that block to the result
+                break
+            elif [r,g,b,a] == [255,0,0,255]: # Checking if it is a 'player_start' block (red)
+                return -1
+                break
+            #checking if this is the last block
+            if block.number+1 is Block.Index:
+                return None
         return result
 
-    def getBlocks(self):
+    '''def getBlocks(self):
         result=[]
-        i=0
-        for dat in self.data:
-            for block in range(len(self.blocks_arr)):
-                if dat is self.blocks_arr[block].number:
-                    res=Block(self.blocks_arr[block].file, self.blocks_arr[block].color)
-                    res.pos=((i%self.height)*res.size[0], (self.height-int(i/self.width))*res.size[1])
-                    print(str(i) + " => " + str(res.pos))
-                    result.append(res)
-                    break
+        counter=0
+        for block_type in self.data:
+            
+            
                 elif dat is -1: # Player block!
-                    plx = i%self.width # Player x = (remainder of i/level_width) times 30 (block size)
+                    plx = counter%self.width # Player x = (remainder of i/level_width) times 30 (block size)
                     #assert (plx < 11), "Player is to far out! ({0})".format(plx) # Player has to be within the first 10 blocks!
-                    self.player_pos = (plx*30, int(i/self.width)*30) # player_pos = (x, y); y = i/level_height, floored to integer, times 30 (block size)
+                    self.player_pos = (plx*30, int(counter/self.width)*30) # player_pos = (x, y); y = i/level_height, floored to integer, times 30 (block size)
                     break
                 if block is len(self.blocks_arr)-1:
                     result.append(None)
-            i+=1
-        self.blocks=result
+            if counter%20 is 0:
+                pass
+            counter+=1
+        self.blocks=result'''
